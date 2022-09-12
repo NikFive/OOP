a60b1c0c2baf1c2b3798168faf780d9dc8a0a419
NikFive <NikIYPT@yandex.ru>
2022-09-12 21:38:18 +0700

    task-1-1-1/add tests and fix code style

--- task-1-1-1/src/test/kotlin/MyHeapSortTest.kt
+++ task-1-1-1/src/test/kotlin/MyHeapSortTest.kt
@@ -1,7 +1,9 @@
-import kotlin.test.*
+import kotlin.test.Test
+import kotlin.test.assertContentEquals
 
 internal class MyHeapSortTest {
     private val testHeapSort: MyHeapSort = MyHeapSort()
+
     @Test
     fun test_one() {
         val input = intArrayOf(5, 4, 3, 2, 1)
@@ -22,6 +24,22 @@ internal class MyHeapSortTest {
     fun test_three() {
         val input = intArrayOf(1, 1, 1, 1)
         val answer = intArrayOf(1, 1, 1, 1)
+        testHeapSort.heapSort(input)
+        assertContentEquals(answer, input)
+    }
+
+    @Test
+    fun test_four() {
+        val input = intArrayOf(0)
+        val answer = intArrayOf(0)
+        testHeapSort.heapSort(input)
+        assertContentEquals(answer, input)
+    }
+
+    @Test
+    fun test_five() {
+        val input = intArrayOf(-1, 5, -1, 5, 0, 0, -1, 5, -1, 5, 0, 0, -1, 5, -1, 5, 0, 0)
+        val answer = intArrayOf(-1, -1, -1, -1, -1, -1, 0, 0, 0, 0, 0, 0, 5, 5, 5, 5, 5, 5)
         testHeapSort.heapSort(input)
         assertContentEquals(answer, input)
     }
