6046697e9342d6670aacd5f5808710b662bc2e18
NikFive <NikIYPT@yandex.ru>
2022-09-18 14:46:54 +0700

    task-1-1-1/use a companion object functions

--- task-1-1-1/src/main/kotlin/MyHeapSort.kt
+++ task-1-1-1/src/main/kotlin/MyHeapSort.kt
@@ -2,37 +2,35 @@ package ru.nsu.fit.konstantinov.task_1_1_1
 
 class MyHeapSort {
     companion object Factory {
-        fun create(): MyHeapSort = MyHeapSort()
-    }
+        fun heapSort(array: IntArray) {
+            val length = array.size
+            for (i in length - 1 downTo 0) {
+                siftDown(array, length, i)
+            }
+            for (i in length - 1 downTo 0) {
+                val temp = array[0]
+                array[0] = array[i]
+                array[i] = temp
+                siftDown(array, i, 0)
+            }
+        }
 
-    fun heapSort(array: IntArray) {
-        val length = array.size
-        for (i in length - 1 downTo 0) {
-            siftDown(array, length, i)
-        }
-        for (i in length - 1 downTo 0) {
-            val temp = array[0]
-            array[0] = array[i]
-            array[i] = temp
-            siftDown(array, i, 0)
-        }
-    }
-
-    private fun siftDown(array: IntArray, n: Int, i: Int) {
-        var max = i
-        val leftChild = 2 * i + 1
-        val rightChild = 2 * i + 2
-        if (leftChild < n && array[leftChild] > array[max]) {
-            max = leftChild
-        }
-        if (rightChild < n && array[rightChild] > array[max]) {
-            max = rightChild
-        }
-        if (max != i) {
-            val temp = array[i]
-            array[i] = array[max]
-            array[max] = temp
-            siftDown(array, n, max)
+        private fun siftDown(array: IntArray, n: Int, i: Int) {
+            var max = i
+            val leftChild = 2 * i + 1
+            val rightChild = 2 * i + 2
+            if (leftChild < n && array[leftChild] > array[max]) {
+                max = leftChild
+            }
+            if (rightChild < n && array[rightChild] > array[max]) {
+                max = rightChild
+            }
+            if (max != i) {
+                val temp = array[i]
+                array[i] = array[max]
+                array[max] = temp
+                siftDown(array, n, max)
+            }
         }
     }
 }
--- task-1-1-1/src/test/kotlin/MyHeapSortTest.kt
+++ task-1-1-1/src/test/kotlin/MyHeapSortTest.kt
@@ -1,15 +1,13 @@
 import org.junit.jupiter.api.Test
-import ru.nsu.fit.konstantinov.task_1_1_1.MyHeapSort
+import ru.nsu.fit.konstantinov.task_1_1_1.MyHeapSort.Factory.heapSort
 import kotlin.test.assertContentEquals
 
 class MyHeapSortTest {
-    private val testHeapSort = MyHeapSort.create()
-
     @Test
     fun testStandard() {
         val input = intArrayOf(101, 351, 166, 767, 923, 971, 524, 695, 8, 61)
         val answer = intArrayOf(8, 61, 101, 166, 351, 524, 695, 767, 923, 971)
-        testHeapSort.heapSort(input)
+        heapSort(input)
         assertContentEquals(answer, input)
     }
 
@@ -17,7 +15,7 @@ class MyHeapSortTest {
     fun testReverse() {
         val input = intArrayOf(5, 4, 3, 2, 1)
         val answer = intArrayOf(1, 2, 3, 4, 5)
-        testHeapSort.heapSort(input)
+        heapSort(input)
         assertContentEquals(answer, input)
     }
 
@@ -25,7 +23,7 @@ class MyHeapSortTest {
     fun testEmpty() {
         val input = intArrayOf()
         val answer = intArrayOf()
-        testHeapSort.heapSort(input)
+        heapSort(input)
         assertContentEquals(answer, input)
     }
 
@@ -33,7 +31,7 @@ class MyHeapSortTest {
     fun testEqual() {
         val input = intArrayOf(1, 1, 1, 1)
         val answer = intArrayOf(1, 1, 1, 1)
-        testHeapSort.heapSort(input)
+        heapSort(input)
         assertContentEquals(answer, input)
     }
 
@@ -41,7 +39,7 @@ class MyHeapSortTest {
     fun testZero() {
         val input = intArrayOf(0)
         val answer = intArrayOf(0)
-        testHeapSort.heapSort(input)
+        heapSort(input)
         assertContentEquals(answer, input)
     }
 
@@ -49,7 +47,7 @@ class MyHeapSortTest {
     fun testNegative() {
         val input = intArrayOf(-1, 5, -1, 5, 0, 0, -1, 5, -1, 5, 0, 0, -1, 5, -1, 5, 0, 0)
         val answer = intArrayOf(-1, -1, -1, -1, -1, -1, 0, 0, 0, 0, 0, 0, 5, 5, 5, 5, 5, 5)
-        testHeapSort.heapSort(input)
+        heapSort(input)
         assertContentEquals(answer, input)
     }
 }
