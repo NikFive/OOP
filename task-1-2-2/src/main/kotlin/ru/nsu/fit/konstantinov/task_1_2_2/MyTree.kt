ddf7b97d2421a48ddafb709837844e9f5941ce37
NikFive <NikIYPT@yandex.ru>
2022-10-09 21:25:49 +0700

    task-1-2-2/add simple realization of tree with DFS, search + tests for it

--- task-1-2-2/src/main/kotlin/ru/nsu/fit/konstantinov/task_1_2_2/MyTree.kt
+++ task-1-2-2/src/main/kotlin/ru/nsu/fit/konstantinov/task_1_2_2/MyTree.kt
@@ -1,5 +1,24 @@
 package ru.nsu.fit.konstantinov.task_1_2_2
 
-class MyTree {
+typealias Visitor<T> = (MyTreeNode<T>) -> Unit
 
+class MyTreeNode<T>(val value: T) {
+    private val children: MutableList<MyTreeNode<T>> = mutableListOf()
+    fun add(child: MyTreeNode<T>) = children.add(child)
+    fun forEachDepthFirst(visit: Visitor<T>) {
+        visit(this)
+        children.forEach {
+            it.forEachDepthFirst(visit)
+        }
+    }
+
+    fun search(value: T): T? {
+        var result: MyTreeNode<T>? = null
+        forEachDepthFirst {
+            if (it.value == value) {
+                result = it
+            }
+        }
+        return result?.value
+    }
 }
--- task-1-2-2/src/test/kotlin/ru/nsu/fit/konstantinov/task_1_2_2/MyTreeTest.kt
+++ task-1-2-2/src/test/kotlin/ru/nsu/fit/konstantinov/task_1_2_2/MyTreeTest.kt
@@ -1,5 +1,41 @@
 package ru.nsu.fit.konstantinov.task_1_2_2
 
-class MyTreeTest {
+import kotlin.test.Test
+import kotlin.test.assertContentEquals
+import kotlin.test.assertEquals
 
+class MyTreeTest {
+    @Test
+    fun testSimple() {
+        val tree = MyTreeNode("numbers")
+        val minus = MyTreeNode("-")
+        val plus = MyTreeNode("+")
+
+        val minusFive = MyTreeNode("-5")
+        val minusTwo = MyTreeNode("-2")
+        val minusTen = MyTreeNode("-10")
+
+        val five = MyTreeNode("5")
+        val two = MyTreeNode("2")
+        val three = MyTreeNode("3")
+
+        tree.add(minus)
+        tree.add(plus)
+
+        minus.add(minusFive)
+        minus.add(minusTwo)
+        minus.add(minusTen)
+
+        plus.add(five)
+        plus.add(two)
+        plus.add(three)
+
+        var checkArray = emptyArray<String>()
+        val exampleArray = arrayOf("numbers", "-", "-5", "-2", "-10", "+", "5", "2", "3")
+        tree.forEachDepthFirst { checkArray += it.value }
+        assertContentEquals(exampleArray, checkArray)
+
+        assertEquals("-5", tree.search("-5"))
+        assertEquals(null, tree.search("0"))
+    }
 }
