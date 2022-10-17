3ff9bc3e6f38e8b10f1ce9bf622816029a25ccca
NikFive <NikFive@users.noreply.github.com>
2022-10-17 18:03:25 +0000

    Deploying to gh-pages from @ NikFive/OOP@e0c83d5b2294660114a9f2874908c7b62473f33d 🚀

--- task-1-2-2/type-search-index.js
+++ task-1-2-2/type-search-index.js
@@ -1,1 +1,1 @@
-var typeSearchIndex = [{"p":"ru.nsu.fit.konstantinov.task_1_2_2","l":"MyTreeNode","url":"ru/nsu/fit/konstantinov/task_1_2_2/MyTreeNode.html"}, {"l":"All classes","url":"allclasses.html"}]
+var typeSearchIndex = [{"p":"ru.nsu.fit.konstantinov.task_1_2_2","l":"MyTree.Node","url":"ru/nsu/fit/konstantinov/task_1_2_2/MyTree.Node.html"}, {"p":"ru.nsu.fit.konstantinov.task_1_2_2","l":"MyTree","url":"ru/nsu/fit/konstantinov/task_1_2_2/MyTree.html"}, {"l":"All classes","url":"allclasses.html"}]
