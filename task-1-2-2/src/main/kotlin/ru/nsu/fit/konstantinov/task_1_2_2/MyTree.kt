1b34e2487193db7fd8fbf4768547c4a4edccc5c3
NikFive <NikIYPT@yandex.ru>
2022-10-18 00:59:27 +0700

    task-1-2-2/create the correct realization of tree

--- task-1-2-2/src/main/kotlin/ru/nsu/fit/konstantinov/task_1_2_2/MyTree.kt
+++ task-1-2-2/src/main/kotlin/ru/nsu/fit/konstantinov/task_1_2_2/MyTree.kt
@@ -1,24 +1,100 @@
 package ru.nsu.fit.konstantinov.task_1_2_2
 
-typealias Visitor<T> = (MyTreeNode<T>) -> Unit
+class MyTree<T>(value: T) : Collection<T> {
+    var root: Node<T>
 
-class MyTreeNode<T>(val value: T) {
-    private val children: MutableList<MyTreeNode<T>> = mutableListOf()
-    fun add(child: MyTreeNode<T>) = children.add(child)
-    fun forEachDepthFirst(visit: Visitor<T>) {
-        visit(this)
-        children.forEach {
-            it.forEachDepthFirst(visit)
-        }
+    init {
+        this.root = Node(value)
     }
 
-    fun search(value: T): T? {
-        var result: MyTreeNode<T>? = null
-        forEachDepthFirst {
-            if (it.value == value) {
+    fun add(node: Node<T>, element: T) {
+        val newElem = Node(element)
+        node.children.add(newElem)
+    }
+
+    fun add(element: T): Node<T> {
+        val newElem = Node(element)
+        root.children.add(newElem)
+        return newElem
+    }
+
+    /**
+     * Returns the size of the collection.
+     */
+    override val size: Int
+        get() = root.children.size
+
+    /**
+     * Returns `true` if the collection is empty (contains no elements), `false` otherwise.
+     */
+    override fun isEmpty(): Boolean {
+        return root.value == null
+    }
+
+    override fun iterator(): Iterator<T> {
+        return DFSIterator(root)
+    }
+
+    /**
+     * Checks if all elements in the specified collection are contained in this collection.
+     */
+    override fun containsAll(elements: Collection<T>): Boolean {
+        for (i in elements) {
+            if (!contains(i)) {
+                return false
+            }
+        }
+        return true
+    }
+
+    /**
+     * Checks if the specified element is contained in this collection.
+     */
+    override fun contains(element: T): Boolean {
+        var result: Node<T>? = null
+        root.forEachDepthFirst {
+            if (it.value == element) {
                 result = it
             }
         }
-        return result?.value
+        return result != null
+    }
+
+    private class DFSIterator<T>(root: Node<T>?) : Iterator<T> {
+        var nodesToVisit: ArrayDeque<Node<T>> = ArrayDeque()
+
+        init {
+            root?.forEachDepthFirst { nodesToVisit.add(it) }
+        }
+
+        /**
+         * Returns `true` if the iteration has more elements.
+         */
+        override fun hasNext(): Boolean {
+            return !nodesToVisit.isEmpty()
+        }
+
+        /**
+         * Returns the next element in the iteration.
+         */
+        override fun next(): T {
+            return this.nextNode().value
+        }
+
+        fun nextNode(): Node<T> {
+            check(hasNext())
+            return nodesToVisit.removeFirst()
+        }
+
+    }
+
+    class Node<T>(val value: T) {
+        val children: MutableList<Node<T>> = mutableListOf()
+        fun forEachDepthFirst(visit: (Node<T>) -> Unit) {
+            visit(this)
+            children.forEach {
+                it.forEachDepthFirst(visit)
+            }
+        }
     }
 }
--- task-1-2-2/src/test/kotlin/ru/nsu/fit/konstantinov/task_1_2_2/MyTreeTest.kt
+++ task-1-2-2/src/test/kotlin/ru/nsu/fit/konstantinov/task_1_2_2/MyTreeTest.kt
@@ -7,35 +7,15 @@ import kotlin.test.assertEquals
 class MyTreeTest {
     @Test
     fun testSimple() {
-        val tree = MyTreeNode("numbers")
-        val minus = MyTreeNode("-")
-        val plus = MyTreeNode("+")
-
-        val minusFive = MyTreeNode("-5")
-        val minusTwo = MyTreeNode("-2")
-        val minusTen = MyTreeNode("-10")
-
-        val five = MyTreeNode("5")
-        val two = MyTreeNode("2")
-        val three = MyTreeNode("3")
-
-        tree.add(minus)
-        tree.add(plus)
-
-        minus.add(minusFive)
-        minus.add(minusTwo)
-        minus.add(minusTen)
-
-        plus.add(five)
-        plus.add(two)
-        plus.add(three)
-
-        var checkArray = emptyArray<String>()
-        val exampleArray = arrayOf("numbers", "-", "-5", "-2", "-10", "+", "5", "2", "3")
-        tree.forEachDepthFirst { checkArray += it.value }
-        assertContentEquals(exampleArray, checkArray)
-
-        assertEquals("-5", tree.search("-5"))
-        assertEquals(null, tree.search("0"))
+        val tree = MyTree(1)
+        tree.add(2)
+        val nodeB = tree.add(3)
+        tree.add(nodeB,4)
+        var array = emptyArray<Int>()
+        val arrayRes = arrayOf(1, 2, 3, 4)
+        for (i in tree) {
+            array += i
+        }
+        assertContentEquals(array, arrayRes)
     }
 }
