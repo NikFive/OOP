a5523ecd6fd37bf27a4931c890ae5abb261ff25a
NikFive <NikFive@users.noreply.github.com>
2022-10-23 12:28:51 +0000

    Deploying to gh-pages from @ NikFive/OOP@4046efac1bf45535dbb3cd4c330be615790902b3 🚀

---
+++ task-1-2-3/package-search-index.js
@@ -0,0 +1,1 @@
+var packageSearchIndex = [{"l":"ru.nsu.fit.konstantinov.task_1_2_3","url":"ru/nsu/fit/konstantinov/task_1_2_3/package-summary.html"}, {"l":"All packages","url":"index.html"}]
---
+++ task-1-2-3/type-search-index.js
@@ -0,0 +1,1 @@
+var typeSearchIndex = [{"p":"ru.nsu.fit.konstantinov.task_1_2_3","l":"MyGraph","url":"ru/nsu/fit/konstantinov/task_1_2_3/MyGraph.html"}, {"l":"All classes","url":"allclasses.html"}]
