9e6e00b160f22ee06a8e5141a7a4fce176fac6cd
NikFive <NikIYPT@yandex.ru>
2022-10-23 19:26:25 +0700

    task-1-2-3/add simple implementation + dfs and bfs

--- task-1-2-3/src/main/kotlin/ru/nsu/fit/konstantinov/task_1_2_3/MyGraph.kt
+++ task-1-2-3/src/main/kotlin/ru/nsu/fit/konstantinov/task_1_2_3/MyGraph.kt
@@ -1,5 +1,50 @@
 package ru.nsu.fit.konstantinov.task_1_2_3
 
 class MyGraph<T> {
+    private data class Vertex<T>(val name: T) {
+        val neighbors = mutableSetOf<Vertex<T>>()
+    }
 
+    private val vertices = mutableMapOf<T, Vertex<T>>()
+    private fun connect(first: Vertex<T>?, second: Vertex<T>?) {
+        if (second != null) first?.neighbors?.add(second)
+        if (first != null) second?.neighbors?.add(first)
+    }
+
+    fun addVertex(name: T) {
+        vertices[name] = Vertex(name)
+    }
+
+    fun connect(first: T, second: T) = connect(vertices[first], vertices[second])
+
+    fun neighbors(name: T): List<T> = vertices[name]?.neighbors?.map { it.name } ?: listOf()
+
+    fun dfs(start: T, finish: T) = dfs(start, finish, setOf())
+
+    private fun dfs(start: T, finish: T, visited: Set<T>): Int = if (start == finish) 0
+    else {
+        val min = neighbors(start).filter {
+            it !in visited
+        }.mapNotNull {
+            dfs(it, finish, visited + start)
+        }.min()
+        min + 1
+    }
+
+    fun bfs(start: T, finish: T): Int {
+        val queue = ArrayDeque<T>()
+        queue.add(start)
+        val visited = mutableMapOf(start to 0)
+        while (queue.isNotEmpty()) {
+            val next = queue.removeFirst()
+            val distance = visited[next]!!
+            if (next == finish) return distance
+            for (neighbor in neighbors(next)) {
+                if (neighbor in visited) continue
+                visited[neighbor] = distance + 1
+                queue.add(neighbor)
+            }
+        }
+        return -1
+    }
 }
--- task-1-2-3/src/test/kotlin/ru/nsu/fit/konstantinov/task_1_2_3/MyGraphTest.kt
+++ task-1-2-3/src/test/kotlin/ru/nsu/fit/konstantinov/task_1_2_3/MyGraphTest.kt
@@ -1,9 +1,48 @@
 package ru.nsu.fit.konstantinov.task_1_2_3
 
-import org.junit.jupiter.api.Assertions.assertThrows
 import kotlin.test.Test
+import kotlin.test.assertContentEquals
 import kotlin.test.assertEquals
 
 class MyGraphTest {
+    @Test
+    fun testSimple() {
+        val graph = MyGraph<String>()
+        graph.addVertex("A")
+        graph.addVertex("B")
+        graph.addVertex("C")
+        graph.addVertex("D")
+        graph.connect("A", "C")
+        graph.connect("B", "D")
+        graph.connect("B", "C")
+        assertContentEquals(listOf("D", "C"), graph.neighbors("B"))
+    }
 
+    @Test
+    fun testDFS() {
+        val graph = MyGraph<String>()
+        graph.addVertex("A")
+        graph.addVertex("B")
+        graph.addVertex("C")
+        graph.addVertex("D")
+        graph.connect("A", "B")
+        graph.connect("B", "C")
+        graph.connect("C", "D")
+        assertEquals(3, graph.dfs("A", "D"))
+        assertEquals(0, graph.dfs("A", "A"))
+    }
+
+    @Test
+    fun testBFS() {
+        val graph = MyGraph<String>()
+        graph.addVertex("A")
+        graph.addVertex("B")
+        graph.addVertex("C")
+        graph.addVertex("D")
+        graph.connect("A", "B")
+        graph.connect("B", "C")
+        graph.connect("C", "D")
+        assertEquals(3, graph.bfs("A", "D"))
+        assertEquals(0, graph.bfs("A", "A"))
+    }
 }
