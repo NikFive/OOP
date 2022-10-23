8b26699ffda0c070eeb2c96ebbad0007b07bd73a
NikFive <NikIYPT@yandex.ru>
2022-10-23 19:03:31 +0700

    task-1-2-3/initial commit

---
+++ task-1-2-3/build.gradle.kts
@@ -0,0 +1,75 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.10"
+    id("org.jetbrains.dokka") version "1.7.10"
+    jacoco
+    application
+}
+
+group = "ru.nsu.fit.konstantinov.task-1-2-3"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    dokkaJavadocPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.7.10")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+        csv.required.set(false)
+        html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+    }
+}
+
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        rule {
+            limit {
+                minimum = "0.5".toBigDecimal()
+            }
+        }
+
+        rule {
+            isEnabled = false
+            element = "CLASS"
+            includes = listOf("org.gradle.*")
+
+            limit {
+                counter = "LINE"
+                value = "TOTALCOUNT"
+                maximum = "0.3".toBigDecimal()
+            }
+        }
+    }
+}
+
+jacoco {
+    toolVersion = "0.8.8"
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.dokkaJavadoc.configure {
+    outputDirectory.set(file("javadoc"))
+}
+
+application {
+    mainClass.set("MainKt")
+}
---
+++ task-1-2-3/src/test/kotlin/ru/nsu/fit/konstantinov/task_1_2_3/MyGraphTest.kt
@@ -0,0 +1,9 @@
+package ru.nsu.fit.konstantinov.task_1_2_3
+
+import org.junit.jupiter.api.Assertions.assertThrows
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class MyGraphTest {
+
+}
