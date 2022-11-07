890539937060490483a8dd9b1887183936f038de
NikFive <NikIYPT@yandex.ru>
2022-11-07 16:23:51 +0700

    task-1-3-1/add file analyze

--- task-1-3-1/src/test/kotlin/ru/nsu/fit/konstantinov/task_1_3_1/FindSubStringTest.kt
+++ task-1-3-1/src/test/kotlin/ru/nsu/fit/konstantinov/task_1_3_1/FindSubStringTest.kt
@@ -3,12 +3,11 @@ package ru.nsu.fit.konstantinov.task_1_3_1
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-
 class FindSubStringTest {
     @Test
     fun testSimple() {
-        val txt = "Я ем пирог!"
-        val pat = "пирог"
-        assertEquals(FindSubString().kmpAlgorithm(pat, txt), arrayListOf(5))
+        val subString = "три"
+        val path = "src/test/kotlin/ru/nsu/fit/konstantinov/task_1_3_1/input.txt"
+        assertEquals(FindSubString().findSubStringInFile(path, subString), arrayListOf(8, 32, 54, 76, 106, 128, 150, 172, 198, 220, 242, 264, 286, 310, 332, 354, 376, 402, 424, 446, 468))
     }
 }
