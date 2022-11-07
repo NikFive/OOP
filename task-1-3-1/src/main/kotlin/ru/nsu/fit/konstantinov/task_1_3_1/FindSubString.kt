4a20e25537d31b953fd3d21f61b6ff2ee3e79a42
NikFive <NikIYPT@yandex.ru>
2022-11-07 14:18:57 +0700

    task-1-3-1/add KMP Algorithm and simple test

--- task-1-3-1/src/main/kotlin/ru/nsu/fit/konstantinov/task_1_3_1/FindSubString.kt
+++ task-1-3-1/src/main/kotlin/ru/nsu/fit/konstantinov/task_1_3_1/FindSubString.kt
@@ -1,5 +1,48 @@
 package ru.nsu.fit.konstantinov.task_1_3_1
 
 class FindSubString {
+    fun kmpAlgorithm(subString: String, string: String): ArrayList<Int> {
+        val result = arrayListOf<Int>()
+        val subStringLength = subString.length
+        val stringLength = string.length
 
+        val prefixArray = IntArray(subStringLength)
+        computePrefixArray(subString, subStringLength, prefixArray)
+
+        var j = 0
+        var i = 0
+        while (stringLength - i >= subStringLength - j) {
+            if (subString[j] == string[i]) {
+                j++
+                i++
+            }
+            if (j == subStringLength) {
+                result.add(i - j)
+                j = prefixArray[j - 1]
+            } else if (i < stringLength && subString[j] != string[i]) {
+                if (j != 0) j = prefixArray[j - 1] else i += 1
+            }
+        }
+        return result
+    }
+
+    private fun computePrefixArray(subString: String, stringLength: Int, prefixArray: IntArray) {
+        var length = 0
+        var count = 1
+        prefixArray[0] = 0
+        while (count < stringLength) {
+            if (subString[count] == subString[length]) {
+                length++
+                prefixArray[count] = length
+                count++
+            } else {
+                if (length != 0) {
+                    length = prefixArray[length - 1]
+                } else {
+                    prefixArray[count] = 0
+                    count++
+                }
+            }
+        }
+    }
 }
--- task-1-3-1/src/test/kotlin/ru/nsu/fit/konstantinov/task_1_3_1/FindSubStringTest.kt
+++ task-1-3-1/src/test/kotlin/ru/nsu/fit/konstantinov/task_1_3_1/FindSubStringTest.kt
@@ -1,11 +1,14 @@
 package ru.nsu.fit.konstantinov.task_1_3_1
 
-import org.junit.jupiter.api.Assertions
-import java.io.FileNotFoundException
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 
 class FindSubStringTest {
-
+    @Test
+    fun testSimple() {
+        val txt = "Я ем пирог!"
+        val pat = "пирог"
+        assertEquals(FindSubString().kmpAlgorithm(pat, txt), arrayListOf(5))
+    }
 }
