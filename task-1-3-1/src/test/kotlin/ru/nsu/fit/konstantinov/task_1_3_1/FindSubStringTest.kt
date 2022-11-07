e22fa6b829de369c571bb5076473cc115a1446b0
NikFive <NikIYPT@yandex.ru>
2022-11-07 18:32:17 +0700

    task-1-3-1/add empty test

--- task-1-3-1/src/test/kotlin/ru/nsu/fit/konstantinov/task_1_3_1/FindSubStringTest.kt
+++ task-1-3-1/src/test/kotlin/ru/nsu/fit/konstantinov/task_1_3_1/FindSubStringTest.kt
@@ -7,7 +7,13 @@ class FindSubStringTest {
     @Test
     fun testSimple() {
         val subString = "три"
+        val subString2 = "ноль"
         val path = "src/test/kotlin/ru/nsu/fit/konstantinov/task_1_3_1/input.txt"
-        assertEquals(FindSubString().findSubStringInFile(path, subString), arrayListOf(8, 32, 54, 76, 106, 128, 150, 172, 198, 220, 242, 264, 286, 310, 332, 354, 376, 402, 424, 446, 468))
+        assertEquals(
+            FindSubString().findSubStringInFile(path, subString), arrayListOf(
+                8, 32, 54, 76, 106, 128, 150, 172, 198, 220, 242, 264, 286, 310, 332, 354, 376, 402, 424, 446, 468
+            )
+        )
+        assertEquals(FindSubString().findSubStringInFile(path, subString2), arrayListOf())
     }
 }
