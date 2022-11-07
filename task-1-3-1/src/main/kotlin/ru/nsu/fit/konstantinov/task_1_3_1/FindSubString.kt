de24ea5ddc90ba0c036701678eda437c2cda9d14
NikFive <NikIYPT@yandex.ru>
2022-11-07 13:14:01 +0700

    task-1-3-1/initial commit

---
+++ task-1-3-1/src/main/kotlin/ru/nsu/fit/konstantinov/task_1_3_1/FindSubString.kt
@@ -0,0 +1,5 @@
+package ru.nsu.fit.konstantinov.task_1_3_1
+
+class FindSubString {
+
+}
---
+++ task-1-3-1/src/test/kotlin/ru/nsu/fit/konstantinov/task_1_3_1/FindSubStringTest.kt
@@ -0,0 +1,11 @@
+package ru.nsu.fit.konstantinov.task_1_3_1
+
+import org.junit.jupiter.api.Assertions
+import java.io.FileNotFoundException
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+
+class FindSubStringTest {
+
+}
