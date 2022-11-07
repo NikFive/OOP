4202057c9f0146bc6fbcb0ff383ae4e8650c3e92
NikFive <NikIYPT@yandex.ru>
2022-11-07 16:23:51 +0700

    task-1-3-1/add file analyze

--- task-1-3-1/src/main/kotlin/ru/nsu/fit/konstantinov/task_1_3_1/FindSubString.kt
+++ task-1-3-1/src/main/kotlin/ru/nsu/fit/konstantinov/task_1_3_1/FindSubString.kt
@@ -1,7 +1,32 @@
 package ru.nsu.fit.konstantinov.task_1_3_1
 
+import java.io.BufferedReader
+import java.io.File
+
 class FindSubString {
-    fun kmpAlgorithm(subString: String, string: String): ArrayList<Int> {
+    fun findSubStringInFile(path: String, subString: String): ArrayList<Int> {
+        val result = arrayListOf<Int>()
+        val bufferedReader: BufferedReader = File(path).bufferedReader()
+        var currentLen = 0
+        var size = 0
+        val bufferSize = 5
+        val buffer = CharArray(bufferSize)
+        var ending = ""
+        while (size != -1) {
+            size = bufferedReader.read(buffer)
+            val string = ending + String(buffer)
+            ending = buffer.drop(subString.length).joinToString("")
+            result += if (currentLen != 0) {
+                kmpAlgorithm(subString, string, currentLen - subString.length + 1)
+            } else {
+                kmpAlgorithm(subString, string, 0)
+            }
+            currentLen += bufferSize
+        }
+        return result
+    }
+
+    private fun kmpAlgorithm(subString: String, string: String, shift: Int): ArrayList<Int> {
         val result = arrayListOf<Int>()
         val subStringLength = subString.length
         val stringLength = string.length
@@ -17,7 +42,7 @@ class FindSubString {
                 i++
             }
             if (j == subStringLength) {
-                result.add(i - j)
+                result.add(i - j + shift)
                 j = prefixArray[j - 1]
             } else if (i < stringLength && subString[j] != string[i]) {
                 if (j != 0) j = prefixArray[j - 1] else i += 1
