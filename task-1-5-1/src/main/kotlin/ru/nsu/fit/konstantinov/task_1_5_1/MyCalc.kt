4cc62cf53517491eb416f753feb2d82b50719473
NikFive <NikIYPT@yandex.ru>
2022-12-05 13:14:14 +0700

    task-1-5-1/add fabric pattern

--- task-1-5-1/src/main/kotlin/ru/nsu/fit/konstantinov/task_1_5_1/MyCalc.kt
+++ task-1-5-1/src/main/kotlin/ru/nsu/fit/konstantinov/task_1_5_1/MyCalc.kt
@@ -2,5 +2,22 @@ package ru.nsu.fit.konstantinov.task_1_5_1
 
 
 class MyCalc {
+    interface CalcOperation {
+        companion object {
+            fun operation(operationName: String, first: Double, second: Double) = when (operationName) {
+                "+" -> PlusClass(first, second).operation()
+                "-" -> MinusClass(first, second).operation()
+                else -> throw Exception("I don't know how to deal with $operationName.")
+            }
+        }
+
+        class PlusClass(private val first: Double, private val second: Double) : CalcOperation {
+            fun operation() = first + second
+        }
+
+        class MinusClass(private val first: Double, private val second: Double) : CalcOperation {
+            fun operation() = first - second
+        }
+    }
 
 }
--- task-1-5-1/src/test/kotlin/ru/nsu/fit/konstantinov/task_1_5_1/MyCalcTest.kt
+++ task-1-5-1/src/test/kotlin/ru/nsu/fit/konstantinov/task_1_5_1/MyCalcTest.kt
@@ -1,5 +1,16 @@
 package ru.nsu.fit.konstantinov.task_1_5_1
 
-class MyCalcTest {
+import org.junit.jupiter.api.Assertions
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
+class MyCalcTest {
+    @Test
+    fun test() {
+        assertEquals(5.0, MyCalc.CalcOperation.operation("+", 2.0, 3.0))
+        assertEquals(-1.0, MyCalc.CalcOperation.operation("-", 2.0, 3.0))
+        Assertions.assertThrows(Exception::class.java) {
+            MyCalc.CalcOperation.operation("A", 2.0, 3.0)
+        }
+    }
 }
