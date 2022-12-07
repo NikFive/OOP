99233c293e862bebd4e5ae95a768bc6d204c4d5b
NikFive <NikIYPT@yandex.ru>
2022-12-07 12:13:32 +0700

    task-1-5-1/implement HashMap Factory

--- task-1-5-1/src/main/kotlin/ru/nsu/fit/konstantinov/task_1_5_1/MyCalc.kt
+++ task-1-5-1/src/main/kotlin/ru/nsu/fit/konstantinov/task_1_5_1/MyCalc.kt
@@ -1,9 +1,21 @@
 package ru.nsu.fit.konstantinov.task_1_5_1
 
 import java.util.*
+import kotlin.math.*
 
 class MyCalc {
     companion object {
+        private val binaryOperations: HashMap<String, (first: Double, second: Double) -> Double> =
+            hashMapOf(Pair("+") { first: Double, second: Double -> first + second },
+                Pair("-") { first: Double, second: Double -> first - second },
+                Pair("/") { first: Double, second: Double -> first / second },
+                Pair("*") { first: Double, second: Double -> first * second },
+                Pair("pow") { first: Double, second: Double -> first.pow(second) },
+                Pair("log") { first: Double, second: Double -> log(first, second) })
+        private val unaryOperations: HashMap<String, (first: Double) -> Double> =
+            hashMapOf(Pair("sin") { first: Double -> sin(first) },
+                Pair("cos") { first: Double -> cos(first) },
+                Pair("sqrt") { first: Double -> sqrt(first) })
         fun calculate(input: String): Double {
             var result = 0.0
             val valueStack = Stack<Double>()
@@ -12,39 +24,17 @@ class MyCalc {
                 if (i.toDoubleOrNull() != null) {
                     valueStack.add(i.toDouble())
                 } else {
-                    result = CalcOperation.operation(i, valueStack.pop(), valueStack.pop())
+                    result = if (binaryOperations.containsKey(i)) {
+                        binaryOperations[i]?.let { it(valueStack.pop(), valueStack.pop()) }!!
+                    } else if (unaryOperations.containsKey(i)) {
+                        unaryOperations[i]?.let { it(valueStack.pop()) }!!
+                    } else {
+                        throw Exception("Unknown operator")
+                    }
                     valueStack.add(result)
                 }
             }
             return result
         }
-    }
-}
-
-interface CalcOperation {
-    companion object {
-        fun operation(operationName: String, first: Double, second: Double) = when (operationName) {
-            "+" -> PlusClass(first, second).operation()
-            "-" -> MinusClass(first, second).operation()
-            "*" -> MultiplyClass(first, second).operation()
-            "/" -> DivideClass(first, second).operation()
-            else -> throw Exception("I don't know how to deal with $operationName.")
-        }
-    }
-
-    class PlusClass(private val first: Double, private val second: Double) : CalcOperation {
-        fun operation() = first + second
-    }
-
-    class MinusClass(private val first: Double, private val second: Double) : CalcOperation {
-        fun operation() = first - second
-    }
-
-    class MultiplyClass(private val first: Double, private val second: Double) : CalcOperation {
-        fun operation() = first * second
-    }
-
-    class DivideClass(private val first: Double, private val second: Double) : CalcOperation {
-        fun operation() = first / second
     }
 }
--- task-1-5-1/src/test/kotlin/ru/nsu/fit/konstantinov/task_1_5_1/MyCalcTest.kt
+++ task-1-5-1/src/test/kotlin/ru/nsu/fit/konstantinov/task_1_5_1/MyCalcTest.kt
@@ -13,6 +13,11 @@ class MyCalcTest {
         assertEquals(0.0, MyCalc.calculate("* - 3.0 3.0 2.0"))
         assertEquals(4.0, MyCalc.calculate("/ + 5.0 3.0 2.0"))
         assertEquals(9.0, MyCalc.calculate("+ - 64 * 15 4 / 20 4"))
+        assertEquals(81.0, MyCalc.calculate("pow + - 64 * 15 4 / 20 4 2"))
+        assertEquals(2.0, MyCalc.calculate("log + - 64 * 15 4 / 20 4 3"))
+        assertEquals(3.0, MyCalc.calculate("sqrt + - 64 * 15 4 / 20 4 3"))
+        assertEquals(0.0, MyCalc.calculate("sin + - 1 2 1"))
+        assertEquals(1.0, MyCalc.calculate("cos + - 1 2 1"))
         Assertions.assertThrows(Exception::class.java) {
             MyCalc.calculate("L 2.0 3.0")
         }
