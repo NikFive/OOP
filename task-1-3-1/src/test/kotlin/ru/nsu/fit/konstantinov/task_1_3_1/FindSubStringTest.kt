3d7d17799e14eaa443b44d7f4c057ca1f5dccea7
NikFive <NikIYPT@yandex.ru>
2022-12-26 11:22:18 +0700

    task-1-3-1/use classloader to load file

--- task-1-3-1/src/test/kotlin/ru/nsu/fit/konstantinov/task_1_3_1/FindSubStringTest.kt
+++ task-1-3-1/src/test/kotlin/ru/nsu/fit/konstantinov/task_1_3_1/FindSubStringTest.kt
@@ -1,26 +1,43 @@
 package ru.nsu.fit.konstantinov.task_1_3_1
 
-import java.io.File
+import org.junit.jupiter.api.BeforeEach
+import java.io.InputStream
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class FindSubStringTest {
+    private var stream: InputStream? = null
+
+    @BeforeEach
+    fun createStream() {
+        stream = ClassLoader.getSystemResourceAsStream("input.txt")
+    }
+
     @Test
-    fun testSimple() {
+    fun testSimpleKMP() {
         val subString = "thr"
+        assertEquals(arrayListOf(
+            8, 37, 62, 82, 107, 125, 150, 175, 200, 219, 244, 266, 291, 309, 334, 420, 445
+        ), stream?.let { FindSubString().findSubStringInStreamKMP(it, subString) })
+    }
+
+    @Test
+    fun testSimpleAho() {
+        val subString = "thr"
+        assertEquals(arrayListOf(
+            8, 37, 62, 82, 107, 125, 150, 175, 200, 219, 244, 266, 291, 309, 334, 420, 445
+        ), stream?.let { FindSubString().findSubStringInStreamAho(it, subString) })
+    }
+
+    @Test
+    fun testEmptyKMP() {
         val subString2 = "zero"
-        val path = "src/test/resources/input.txt"
-        assertEquals(
-            arrayListOf(
-                8, 37, 62, 82, 107, 125, 150, 175, 200, 219, 244, 266, 291, 309, 334, 420, 445
-            ), FindSubString().findSubStringInStreamKMP(File(path).inputStream(), subString)
-        )
-        assertEquals(
-            arrayListOf(
-                8, 37, 62, 82, 107, 125, 150, 175, 200, 219, 244, 266, 291, 309, 334, 420, 445
-            ), FindSubString().findSubStringInStreamAho(File(path).inputStream(), subString)
-        )
-        assertEquals(FindSubString().findSubStringInStreamKMP(File(path).inputStream(), subString2), arrayListOf())
-        assertEquals(FindSubString().findSubStringInStreamAho(File(path).inputStream(), subString2), arrayListOf())
+        assertEquals(stream?.let { FindSubString().findSubStringInStreamKMP(it, subString2) }, arrayListOf())
+    }
+
+    @Test
+    fun testEmptyAho() {
+        val subString2 = "zero"
+        assertEquals(stream?.let { FindSubString().findSubStringInStreamKMP(it, subString2) }, arrayListOf())
     }
 }
