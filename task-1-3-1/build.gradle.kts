9dff0f1a334ce6900a995805aa28281ee4e3948d
NikFive <NikIYPT@yandex.ru>
2022-12-26 11:47:18 +0700

    task-1-3-1/try to fix build 2

--- task-1-3-1/build.gradle.kts
+++ task-1-3-1/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.10" apply false
+    kotlin("jvm") version "1.7.10"
     id("org.jetbrains.dokka") version "1.7.10"
     jacoco
     application
