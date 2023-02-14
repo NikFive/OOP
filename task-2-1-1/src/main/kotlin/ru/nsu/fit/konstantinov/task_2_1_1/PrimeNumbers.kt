1a786111e60626a8b153510dad4fbf1a63514bd3
NikFive <NikIYPT@yandex.ru>
2023-02-14 07:24:52 +0700

    task-2-1-1/add serial realization

--- task-2-1-1/src/main/kotlin/ru/nsu/fit/konstantinov/task_2_1_1/PrimeNumbers.kt
+++ task-2-1-1/src/main/kotlin/ru/nsu/fit/konstantinov/task_2_1_1/PrimeNumbers.kt
@@ -1,4 +1,27 @@
 package ru.nsu.fit.konstantinov.task_2_1_1
 
+import kotlin.math.sqrt
 
-class PrimeNumbers
+
+class PrimeNumbers {
+    private fun isPrimeNumber(number: Int): Boolean {
+        var result = false
+        for (i in 2..sqrt(number.toDouble()).toInt() + 1) {
+            if (number % i == 0) {
+                result = true
+                break
+            }
+        }
+        return result
+    }
+    fun containsPrimeNumbersSerial(array: ArrayList<Int>): Boolean {
+        var result = false
+        for (number in array) {
+            result = isPrimeNumber(number)
+            if (result) {
+                break
+            }
+        }
+        return result
+    }
+}
