d377a70d0814ea4330851edffbd82f411ac084e1
NikFive <NikIYPT@yandex.ru>
2023-02-14 10:12:57 +0700

    task-2-1-1/add benchmarks (not working now)

--- task-2-1-1/src/main/kotlin/ru/nsu/fit/konstantinov/task_2_1_1/PrimeNumbers.kt
+++ task-2-1-1/src/main/kotlin/ru/nsu/fit/konstantinov/task_2_1_1/PrimeNumbers.kt
@@ -1,27 +1,59 @@
 package ru.nsu.fit.konstantinov.task_2_1_1
 
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import kotlin.math.sqrt
 
 
 class PrimeNumbers {
-    private fun isPrimeNumber(number: Int): Boolean {
-        var result = false
-        for (i in 2..sqrt(number.toDouble()).toInt() + 1) {
-            if (number % i == 0) {
-                result = true
-                break
+    companion object {
+        private fun isPrimeNumber(number: Int): Boolean {
+            var result = false
+            for (i in 2..sqrt(number.toDouble()).toInt() + 1) {
+                if (number % i == 0) {
+                    result = true
+                    break
+                }
             }
+            return result
         }
-        return result
-    }
-    fun containsPrimeNumbersSerial(array: ArrayList<Int>): Boolean {
-        var result = false
-        for (number in array) {
-            result = isPrimeNumber(number)
-            if (result) {
-                break
+
+        private fun isPrimeNumberSuspend(number: Int): Boolean {
+            var result = false
+            for (i in 2..sqrt(number.toDouble()).toInt() + 1) {
+                if (number % i == 0) {
+                    result = true
+                    break
+                }
             }
+            return result
         }
-        return result
+
+        fun containsPrimeNumbersSerial(array: ArrayList<Int>): Boolean {
+            var result = false
+            for (number in array) {
+                result = isPrimeNumber(number)
+                if (result) {
+                    break
+                }
+            }
+            return result
+        }
+
+
+        fun containsPrimeNumbersParallel(array: ArrayList<Int>): Boolean {
+            var result = false
+            runBlocking {
+                launch {
+                    for (number in array) {
+                        result = isPrimeNumber(number)
+                        if (result) {
+                            break
+                        }
+                    }
+                }
+            }
+            return result
+        }
     }
 }
--- task-2-1-1/src/test/kotlin/ru/nsu/fit/konstantinov/task_2_1_1/PrimeNumbersTest.kt
+++ task-2-1-1/src/test/kotlin/ru/nsu/fit/konstantinov/task_2_1_1/PrimeNumbersTest.kt
@@ -1,4 +1,34 @@
 package ru.nsu.fit.konstantinov.task_2_1_1
 
+import org.openjdk.jmh.annotations.*
+import ru.nsu.fit.konstantinov.task_2_1_1.PrimeNumbers.Companion.containsPrimeNumbersParallel
+import ru.nsu.fit.konstantinov.task_2_1_1.PrimeNumbers.Companion.containsPrimeNumbersSerial
+import java.util.concurrent.TimeUnit
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
-class PrimeNumbersTest
+@State(Scope.Benchmark)
+@Fork(1)
+@Warmup(iterations = 0)
+@Measurement(iterations = 1, time = 1, timeUnit = TimeUnit.SECONDS)
+
+class PrimeNumbersTest {
+
+    @Test
+    @Benchmark
+    fun testSerial() {
+        val testArray1 = arrayListOf(6, 8, 7, 13, 9, 4)
+        val testArray2 = arrayListOf(6997901, 6997927, 6997937, 6997967, 6998009, 6998029, 6998039, 6998051, 6998053)
+        assertEquals(containsPrimeNumbersSerial(testArray1), true)
+        assertEquals(containsPrimeNumbersSerial(testArray2), false)
+    }
+
+    @Test
+    @Benchmark
+    fun testParallel() {
+        val testArray1 = arrayListOf(6, 8, 7, 13, 9, 4)
+        val testArray2 = arrayListOf(6997901, 6997927, 6997937, 6997967, 6998009, 6998029, 6998039, 6998051, 6998053)
+        assertEquals(containsPrimeNumbersParallel(testArray1), true)
+        assertEquals(containsPrimeNumbersParallel(testArray2), false)
+    }
+}
