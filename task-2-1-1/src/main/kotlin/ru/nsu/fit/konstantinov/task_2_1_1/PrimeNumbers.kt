b2faca4ce586b44394caacef54a180cc4676f0ec
NikFive <NikIYPT@yandex.ru>
2023-04-28 14:20:19 +0700

    task-2-1-1/add Threads realization and graphics

--- task-2-1-1/src/main/kotlin/ru/nsu/fit/konstantinov/task_2_1_1/PrimeNumbers.kt
+++ task-2-1-1/src/main/kotlin/ru/nsu/fit/konstantinov/task_2_1_1/PrimeNumbers.kt
@@ -2,6 +2,5 @@ package ru.nsu.fit.konstantinov.task_2_1_1
 
 
 interface PrimeNumbers {
-    suspend fun isPrimeNumber(number: Int): Boolean
     fun containsPrimeNumbers(array: ArrayList<Int>): Boolean
 }
--- task-2-1-1/src/main/kotlin/ru/nsu/fit/konstantinov/task_2_1_1/implementations/ParallelStreamImpl.kt
+++ task-2-1-1/src/main/kotlin/ru/nsu/fit/konstantinov/task_2_1_1/implementations/ParallelStreamImpl.kt
@@ -3,20 +3,9 @@ package ru.nsu.fit.konstantinov.task_2_1_1.implementations
 import ru.nsu.fit.konstantinov.task_2_1_1.PrimeNumbers
 import kotlin.math.sqrt
 
-class ParallelStream {
+class ParallelStreamImpl {
     companion object : PrimeNumbers {
-        override suspend fun isPrimeNumber(number: Int): Boolean {
-            var result = false
-            for (i in 2..sqrt(number.toDouble()).toInt() + 1) {
-                if (number % i == 0) {
-                    result = true
-                    break
-                }
-            }
-            return result
-        }
-
-        private fun isPrimeNumberNotSuspend(number: Int): Boolean {
+        private fun isPrimeNumber(number: Int): Boolean {
             var result = false
             for (i in 2..sqrt(number.toDouble()).toInt() + 1) {
                 if (number % i == 0) {
@@ -28,6 +17,6 @@ class ParallelStream {
         }
 
         override fun containsPrimeNumbers(array: ArrayList<Int>): Boolean =
-            array.parallelStream().anyMatch { isPrimeNumberNotSuspend(it) }
+            array.parallelStream().anyMatch { isPrimeNumber(it) }
     }
 }
